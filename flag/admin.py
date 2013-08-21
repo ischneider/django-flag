bf8428b01565a75f4593d42c37e0bc276f717966
Ian Schneider <planablediglet@gmail.com>
2013-08-21 10:58:28 -0600

    add FlagInstance to admin

--- flag/admin.py
+++ flag/admin.py
@@ -12,4 +12,19 @@ class FlaggedContentAdmin(admin.ModelAdmin):
     inlines = [InlineFlagInstance]
 
 
+def flagged_object_link(flag_instance):
+    content = flag_instance.flagged_content
+    obj = content.content_object
+    print dir(obj._meta)
+    return "<a href='%s'>%s</a>" % (obj.get_absolute_url(), obj._meta.verbose_name)
+flagged_object_link.short_description = 'The Flagged Item'
+flagged_object_link.allow_tags = True
+
+
+class FlagInstanceAdmin(admin.ModelAdmin):
+    list_display = ('user','comment','flag_type', flagged_object_link)
+    list_filter = ('flag_type',)
+
+
 admin.site.register(FlaggedContent, FlaggedContentAdmin)
+admin.site.register(FlagInstance, FlagInstanceAdmin)
