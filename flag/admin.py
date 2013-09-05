aee37247dab48c66b7624f5ec478f482c154504c
Ian Schneider <planablediglet@gmail.com>
2013-09-05 15:19:51 -0600

    make admin work better, add flag_type to form

--- flag/admin.py
+++ flag/admin.py
@@ -1,29 +1,84 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.core import urlresolvers
 
 from flag.models import FlaggedContent, FlagInstance
 
-
-class InlineFlagInstance(admin.TabularInline):
-    model = FlagInstance
-    extra = 0
+_absolute_url_resolvers = {}
+_group_to_flag_type = {}
 
 
-class FlaggedContentAdmin(admin.ModelAdmin):
-    inlines = [InlineFlagInstance]
+def register_absolute_url_resolver(model_class, func):
+    _absolute_url_resolvers[model_class] = func
 
 
-def flagged_object_link(flag_instance):
-    content = flag_instance.flagged_content
+def register_group_to_flag_types(*mappings):
+    _group_to_flag_type.update(mappings)
+
+
+def flagged_object_link(model):
+    content = getattr(model, 'flagged_content', model)
     obj = content.content_object
-    print dir(obj._meta)
-    return "<a href='%s'>%s</a>" % (obj.get_absolute_url(), obj._meta.verbose_name)
+    url = obj.get_absolute_url() if hasattr(obj, 'get_absolute_url') else None
+    if not url:
+        resolver = _absolute_url_resolvers.get(type(obj), None)
+        if resolver:
+            url = resolver(obj)
+    if not url: return ''
+    return "<a href='%s'>%s</a>" % (url, obj._meta.verbose_name)
 flagged_object_link.short_description = 'The Flagged Item'
 flagged_object_link.allow_tags = True
 
 
+def user_link(flag_instance):
+    user = flag_instance.user
+    return "<a href='%s'>%s</a>" % (user.get_absolute_url(), user.username)
+user_link.short_description = 'User'
+user_link.allow_tags = True
+
+
+def flagged_content_link(flag_instance):
+    url = urlresolvers.reverse("admin:flag_flaggedcontent_change",
+                    args=(flag_instance.flagged_content.id,))
+    return "<a href='%s'>%s</a>" % (url, 'Admin Link')
+flagged_content_link.short_description = 'Flagged Content'
+flagged_content_link.allow_tags = True
+
+
+class InlineFlagInstance(admin.TabularInline):
+    readonly_fields = ('user', 'flag_type', 'comment', 'when_added', 'when_recalled')
+    model = FlagInstance
+    extra = 0
+
+
+class FlaggedContentForm(forms.ModelForm):
+    moderator = forms.ModelChoiceField(queryset=
+        User.objects.filter(groups__name__in=_group_to_flag_type.keys()))
+    class Meta:
+        model = FlaggedContent
+
+
+class FlaggedContentAdmin(admin.ModelAdmin):
+    form = FlaggedContentForm
+    exclude = ('content_type', 'object_id')
+    readonly_fields = ('creator', 'count')
+    inlines = [InlineFlagInstance]
+
+
 class FlagInstanceAdmin(admin.ModelAdmin):
-    list_display = ('user','comment','flag_type', flagged_object_link)
+    ordering = ('-when_added', )
+    list_display = ('pk', flagged_content_link, user_link, 'when_recalled',
+                    'when_added', 'comment', 'flag_type', flagged_object_link)
     list_filter = ('flag_type',)
+
+    def queryset(self, request):
+        qs = super(FlagInstanceAdmin, self).queryset(request)
+        groups = request.user.groups.values_list('name', flat=True)
+        for group, flag_type in _group_to_flag_type.items():
+            if group in groups:
+                qs = qs.filter(flag_type=flag_type)
+        return qs
 
 
 admin.site.register(FlaggedContent, FlaggedContentAdmin)
