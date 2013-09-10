e9b6fda61d366c99c72ff14c8e6afe1cc8ab7695
Ian Schneider <planablediglet@gmail.com>
2013-09-10 17:11:45 -0600

    admin flag workflow enhancements
    - more readonly, more links
    - add filter for when_recalled
    - add action to recall

--- flag/admin.py
+++ flag/admin.py
@@ -2,8 +2,11 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.core import urlresolvers
+from django.utils.safestring import mark_safe
 
 from flag.models import FlaggedContent, FlagInstance
+
+from datetime import datetime
 
 _absolute_url_resolvers = {}
 _group_to_flag_type = {}
@@ -46,31 +49,74 @@ flagged_content_link.short_description = 'Flagged Content'
 flagged_content_link.allow_tags = True
 
 
+def flag_link(flag_instance):
+    url = urlresolvers.reverse("admin:flag_flaginstance_change",
+                    args=(flag_instance.id,))
+    return "<a href='%s'>%s</a>" % (url, 'Admin Link')
+flag_link.short_description = 'Link to Flag'
+flag_link.allow_tags = True
+
+
 class InlineFlagInstance(admin.TabularInline):
-    readonly_fields = ('user', 'flag_type', 'comment', 'when_added', 'when_recalled')
+    readonly_fields = ('user', 'flag_type', 'comment', 'when_added', 'when_recalled', flag_link)
     model = FlagInstance
     extra = 0
 
 
 class FlaggedContentForm(forms.ModelForm):
+    flagged_object = forms.CharField(label='Link to Flagged Item')
     moderator = forms.ModelChoiceField(queryset=
         User.objects.filter(groups__name__in=_group_to_flag_type.keys()))
     class Meta:
         model = FlaggedContent
 
+    def __init__(self, *args, **kw):
+        super(FlaggedContentForm, self).__init__(*args, **kw)
+        link = mark_safe(unicode(flagged_object_link(self.instance)))
+        self.fields['flagged_object'].widget.render = lambda *a,**kw: link
+
 
 class FlaggedContentAdmin(admin.ModelAdmin):
     form = FlaggedContentForm
+    list_display = ('pk', 'content_type', 'status', 'count', 'moderator')
+    fields = ('moderator', 'status', 'flagged_object')
     exclude = ('content_type', 'object_id')
     readonly_fields = ('creator', 'count')
     inlines = [InlineFlagInstance]
 
 
+class ActiveFilter(admin.SimpleListFilter):
+    title = 'Active'
+    parameter_name = 'active'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('active', 'Active'),
+            ('resolved', 'Resolved'),
+        )
+
+    def queryset(self, request, queryset):
+        val =  self.value()
+        if val == 'active':
+            queryset = queryset.filter(when_recalled__isnull=True)
+        elif val == 'resolved':
+            queryset = queryset.filter(when_recalled__isnull=False)
+        return queryset
+
+
 class FlagInstanceAdmin(admin.ModelAdmin):
-    ordering = ('-when_added', )
+    ordering = ('-when_recalled', '-when_added')
+    exclude = ('flagged_content', )
     list_display = ('pk', flagged_content_link, user_link, 'when_recalled',
                     'when_added', 'comment', 'flag_type', flagged_object_link)
-    list_filter = ('flag_type',)
+    list_filter = ('flag_type', ActiveFilter)
+    readonly_fields = ('user', 'when_added', 'when_recalled', 'flag_type')
+    actions = ['recall_flag']
+
+    def recall_flag(self, req, qs):
+        # filter any already recalled flags
+        qs = qs.filter(when_recalled__isnull = True)
+        qs.update(when_recalled=datetime.now())
 
     def queryset(self, request):
         qs = super(FlagInstanceAdmin, self).queryset(request)
